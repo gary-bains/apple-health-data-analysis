3b578fa7a847c8822b4e1c49a395f96f00c6b91d
Gurpreet Bains <gurpreetbains@Gurpreets-MacBook-Air.local>
2025-04-27 14:35:30 -0600

    Add parsing for raw data to bronze

---
+++ raw_data_to_bronze_data.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+import json
+import sys
+
+raw_file = 'raw_data/export.xml'
+bronze_file = 'bronze_data/records.jsonl'
+
+
+def parse_health_export() -> tuple[list[dict], list[str]]:
+    records: list[dict] = []
+    record_types: list[str] = []
+
+    for _, elem in ET.iterparse(raw_file):
+        if elem.tag == 'Record':
+            records.append(elem.attrib)
+            if elem.attrib['type'] not in record_types:
+                record_types.append(elem.attrib['type'])
+
+    return records, record_types
+
+
+def write_all_records_json_file(records: list[dict]):
+    with open(bronze_file, 'w') as txt_file:
+        for record in records:
+            txt_file.write(json.dumps(record) + '\n')
+
+
+def main() -> int:
+    records, record_types = parse_health_export()
+    print(record_types)
+
+    print('Writing JSONL file...')
+    write_all_records_json_file(records)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
